c284cfd2c425dc538eece3bb3ce7fdd30ed2e7f8
SURULERE242 <ynwsuru@gmail.com>
2026-04-20 08:56:26 +0100

    Nouveau domaine banditprod

--- wavefx/settings.py
+++ wavefx/settings.py
@@ -9,6 +9,7 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
+    'https://banditprod.up.railway.app',
     'https://web-production-e11aa.up.railway.app',
     'http://127.0.0.1:8000',
     'http://localhost:8000',
